dfd41cc2005209feca5f841596655dc16b2b63de
yangjeewoong <danielay@naver.com>
2013-11-14 08:40:12 +0900

    Add console function by google docs updating. now your cooperator can directly watch compile result :)

--- tester/app/controllers/kct_controller.rb
+++ tester/app/controllers/kct_controller.rb
@@ -15,7 +15,7 @@ class KctController < ApplicationController
             session = GoogleDrive.login("", "")
             file = session.file_by_title("code.c")
             file.download_to_file("public/code")
-
+            
             file = File.new("public/code", "r")
             file_w = File.new("public/code_convert","w")
             
@@ -23,6 +23,7 @@ class KctController < ApplicationController
                 line.gsub!("&nbsp;","")
                 line.gsub!("&ldquo;","&quot;")
                 line.gsub!("&rdquo;","&quot;")
+                line.gsub!("</p>","</p>\n")
                 file_w.syswrite(line)
             end
             
@@ -34,25 +35,41 @@ class KctController < ApplicationController
             log_flag = 0
 
             codewrite = File.new("public/real_code.c", "w")
+            file_w = File.new("public/final_output.txt","w")
             
             doc.xpath('//body[@class]/p/span').each do |a|
                 if a.content == "##"
                     if log_flag == 0
                         log_flag = 1
-                        else
+                    else
                         log_flag = 0
                     end
-                    
-                    elsif log_flag == 1
+                elsif log_flag == 1
                     codewrite.syswrite(a.content)
                     codewrite.syswrite("\n")
                 end
+                file_w.syswrite(a.content);
+                file_w.syswrite("\n")
             end
             
             codewrite.close
-        
-            system("gcc -o public/a.out public/real_code.c")
-            system("./public/a.out > app/views/kct/putdata.html.erb")
-    end
 
+            system("gcc -o public/a.out public/real_code.c")
+            system("./public/a.out > public/c_compile")
+
+            result_file = File.open("public/c_compile","r");
+            result_string = result_file.read
+            file_w.syswrite("// print start \n")
+            file_w.syswrite(result_string+"\n")
+            file_w.syswrite("// print end \n\n")
+            result_file.close
+            file_w.close
+
+            file = session.file_by_title("code.c")
+            file.update_from_file("public/final_output.txt")
+
+            #system("./public/a.out > app/views/kct/putdata.html.erb")
+
+            #system("rm public/real_code.c public/c_compile public/a.out public/final_output public/code public/code_convert")
+    end
 end
